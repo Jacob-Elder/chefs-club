f217d3793e270b9c48bb292295e375fe038a5ee3
Jacob Elder <jacobelder@Jacobs-MacBook-Pro.local>
2023-02-18 01:14:12 -0800

    setting up db.json for development puropses

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './css/index.css'
 
 const App = () => {
+    const [counter, setCounter] = useState(0)
     return (
         <div className="container">
-            hello world
+            hello world<br />
+            count: {counter}<br />
+            <button onClick={() => {setCounter(counter + 1)}}>add 1</button>
         </div>
     )
 }
