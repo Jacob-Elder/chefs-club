db038e0d3aa6d8648bbbd69ca2507a380793f002
Jacob Elder <jacobelder@Jacobs-MacBook-Pro.local>
2023-02-18 23:46:58 -0800

    setup Apollo Server and schema for users and posts

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
+const {GraphQLError} = require('graphql')
 const DB = require("./db.json")
 
 let users = DB.users
@@ -26,12 +27,42 @@ const typeDefs = `
 
     type Query {
         allPosts: [Post!]!
+        findPostById(id: String!): Post
+        findUserById(id: String!): User
     }
 `
 
 const resolvers = {
   Query: {
-    allPosts: (root, args) => posts
+    allPosts: (root, args) => posts,
+    findPostById: (root, args) => {
+      const post = posts.find(p => p.id.toString() === args.id)
+      //throw an error if user does not exist
+      if (!post) {
+        throw new GraphQLError("Could not find post", {
+          extensions: {
+            code: "BAD_USER_INPUT",
+            invalidArgs: args.id
+          }
+        })
+      }
+      // return post
+      return post
+    },
+    findUserById: (root, args) => {
+      const user = users.find(u => u.id.toString() === args.id)
+      //throw an error if user does not exist
+      if (!user) {
+        throw new GraphQLError("Could not find user", {
+          extensions: {
+            code: "BAD_USER_INPUT",
+            invalidArgs: args.id
+          }
+        })
+      }
+      // return user
+      return user
+    }
   }
 }
 
