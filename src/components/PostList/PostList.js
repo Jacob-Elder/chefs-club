0c28a194836163fcdfa8fafa9cc33f8c802f9a0e
Jacob Elder <jacobelder@Jacobs-MacBook-Pro.local>
2023-03-10 20:37:19 -0800

    added thumbsup icon and user to postlist component

--- src/components/PostList/PostList.js
+++ src/components/PostList/PostList.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import {Link} from "react-router-dom"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faThumbsUp } from '@fortawesome/free-solid-svg-icons'
 import "./PostList.css"
 
 const PostList = ({posts}) => {
@@ -7,9 +9,15 @@ const PostList = ({posts}) => {
         <div className='list'>
         {posts.map(post =>
             <div className='post' key={post._id}>
-                <Link to={`/posts/${post._id}`}  className="link"><h2>{post.title}</h2></Link>
-                Likes: {post.likes}<br />
-                Tags: {post.tags.toString()}
+                <div>
+                    <FontAwesomeIcon size="2x" icon={faThumbsUp} className="thumb" /><br />
+                    {post.likes}
+                </div>
+                <div>
+                    <Link to={`/posts/${post._id}`}  className="link">{post.title}</Link><br />
+                    posted by: <Link to={`/users/${post.user._id}`}>{post.user.username}</Link>
+                </div>
+                <div></div>
             </div>
         )}
         </div>
